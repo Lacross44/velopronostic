8af51094faaac35af9bbc19f80bb11d55613fba8
Antoine Crossouard <antoine.crossouard@gmail.com>
2026-03-02 19:04:18 +0100

    add console

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+console.log("ADMIN PAGE LOADED - SUPABASE URL:", process.env.NEXT_PUBLIC_SUPABASE_URL)
 import { useEffect, useState } from "react"
 import { supabase } from "@/lib/supabaseClient"
 
