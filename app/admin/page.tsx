38aec18b7d138f7e907855876ac0b6e75d485c1f
Antoine Crossouard <antoine.crossouard@gmail.com>
2026-03-29 09:50:12 +0200

    modif course up

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -551,10 +551,7 @@ async function updateRace() {
                         Modifier
                         </button>
                       </div>
-                    </div>
-                  </div>
-                ))}
-                {editingRaceId && (
+                       {editingRaceId && (
   <div className="rounded-3xl border border-white/10 bg-white/5 p-6 mt-6">
     <h2 className="text-2xl font-bold mb-4">Modifier la course</h2>
 
@@ -598,6 +595,10 @@ async function updateRace() {
     </div>
   </div>
 )}
+                    </div>
+                  </div>
+                ))}
+               
 
                 {races.length === 0 && (
                   <div className="text-white/60">Aucune course pour le moment.</div>
